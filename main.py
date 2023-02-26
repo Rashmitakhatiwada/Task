691748c9d1117d5e5da3fd8c79575bb46455b79c
Peshal Bhandari <rasmitakhatiwada2@gmail.com>
2023-02-26 15:40:31 +0545

    creating new branch

--- main.py
+++ main.py
@@ -18,3 +18,6 @@ r = input("enter radius of circle")
 area = (22/7) * int(r) ** 2
 print("area of the circle is", area)
 
+
+
+
---
+++ task.py
@@ -0,0 +1,4 @@
+lst = [5,2,3,5,3,2,3,3,1,4]
+l1 = input("enter the value you want to count")
+val=lst.count(int(l1))
+print(val)
