249651efadb39646f951f30fc16c5571eb045f50
Peshal Bhandari <rasmitakhatiwada2@gmail.com>
2023-03-01 16:24:05 +0545

    adding old tasks

---
+++ old task.py
@@ -0,0 +1,9 @@
+r = input("enter radius of circle")
+cir = 2 * (22/7) * int(r)
+print("area of the circle is", cir)
+
+
+l = input("enter length of rectangle")
+b = input("enter breadth of rectangle")
+area = int(l)*int(b)
+print("area of the rectangle is", int(area))
